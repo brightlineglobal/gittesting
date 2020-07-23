7cebbe57c870d042e12d82d363fcec9e3476f19b
brightlineglobal <68163464+brightlineglobal@users.noreply.github.com>
2020-07-23 01:09:26 -0500

    Update GitClassTesting.java

--- src/gittest/GitClassTesting.java
+++ src/gittest/GitClassTesting.java
@@ -13,6 +13,7 @@ public class GitClassTesting {
 		System.out.println("test line 7");
 		System.out.println("test line 8 from github");
 		System.out.println("let's try last one");
+		System.out.println("let's try last last time");
 		
 	}
 }
